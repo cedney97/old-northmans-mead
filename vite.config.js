89a3801e4ecba8ee0c9c96c2ee5c7d90f27e693b
cedney97 <cadeedney@gmail.com>
2025-06-13 23:20:23 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,6 @@ export default defineConfig({
       pages: path.resolve(__dirname, "src/pages"),
       data: path.resolve(__dirname, "src/data")
     }
-  }
+  },
+  base: "/old-northmans-mead/"
 })
