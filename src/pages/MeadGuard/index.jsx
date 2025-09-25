d7b4981dcb73f3754c971a3972412dd4c49ac4bc
cedney97 <cadeedney@gmail.com>
2025-09-25 18:49:19 -0500

    Added mead guard info

--- src/pages/MeadGuard/index.jsx
+++ src/pages/MeadGuard/index.jsx
@@ -2,6 +2,9 @@ import Page from "../../components/template/Page"
 import styles from './MeadGuard.module.scss'
 import meadguard from 'assets/meadguard.png'
 import divider from "assets/divider.png"
+import shirt1 from 'assets/shirt1.jpg'
+import shirt2 from 'assets/shirt2.jpg'
+import tankard from 'assets/tankard.png'
 
 const MeadGuard = () => {
     return (
@@ -11,6 +14,36 @@ const MeadGuard = () => {
             <h2 style={{ marginTop: "-32px" }}>Sign up for the MEAD + GUARD and receive the First of it's Kind, ONM Shirt and Mug plus discounts and More!</h2>
             <img src={divider} className={styles.divider}></img>
             <h1>Mead + Guard Tiers</h1>
+            <div className={styles.points}>
+                <div>
+                    <h3>Tier 1: $100+ Donation</h3>
+                    <p>Members get a shirt, Mead + Guard Tankard, and $5 off their next selection of Old Northmen's Mead!</p>
+                </div>
+                <div>
+                    <h3>Tier 2: $250+ Donation</h3>
+                    <p>Members get a shirt, Mead + Guard Tankard, and their choice of a bottle of Old Northmen's Mead (including the Holiday-Spiced ready for the Holidays)!</p>
+                </div>
+                <div>
+                    <h3>Tier 3: $500+ Donation</h3>
+                    <p>Members get a shirt, Mead + Guard Tankard, and a bottle of each profile of Old Northmen's Mead ready for the Holidays!</p>
+                </div>
+            </div>
+            <h1>Mead + Guard Products</h1>
+            <div className={styles.products}>
+                <div>
+                    <h3>T-Shirt</h3>
+                    <div className={styles.images}>
+                        <img src={shirt1}></img>
+                        <img src={shirt2}></img>
+                    </div>
+                </div>
+                <div>
+                    <h3>Tankard</h3>
+                    <div className={styles.images}>
+                        <img src={tankard}></img>
+                    </div>
+                </div>
+            </div>
         </Page>
     )
 }
